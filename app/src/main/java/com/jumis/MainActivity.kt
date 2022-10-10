1c8f4ed074cf4400a775e1fd95805936be6876e1
JuanjoLQ <jjlopezquinonero@gmail.com>
2022-10-10 11:42:04 +0200

    Toma de contacto en login y main

--- app/src/main/java/com/jumis/MainActivity.kt
+++ app/src/main/java/com/jumis/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.jumis
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.btnLogin)
+        button.setOnClickListener {
+            val intent = Intent(this@MainActivity, Login::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
