b0c05573164da1a54ae9bb599547942ad1f91909
JuanjoLQ <jjlopezquinonero@gmail.com>
2022-10-11 09:54:00 +0200

    Cambio de actividades de Java a Kotlin. Avances en Settings. Login hecho a falta de algun detalle. En el main hay una parte comentada con intents para cambiar de actividades , aún por poner.

--- app/src/main/java/com/jumis/MainActivity.kt
+++ app/src/main/java/com/jumis/MainActivity.kt
@@ -10,13 +10,52 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val algo
+
 
         val button: Button = findViewById(R.id.btnLogin)
         button.setOnClickListener {
             val intent = Intent(this@MainActivity, Login::class.java)
             startActivity(intent)
         }
+
+        val button1: Button = findViewById(R.id.buttonRegister)
+        button1.setOnClickListener {
+            val intent = Intent(this@MainActivity, Register::class.java)
+            startActivity(intent)
+        }
+
+
+        /*
+        Intents para las actividades
+
+        Login --> Register
+        val button: Button = findViewById(R.id.buttonRegister)
+        button.setOnClickListener {
+            val intent = Intent(this@Login, Register::class.java)
+            startActivity(intent)
+        }
+
+        Register --> Login
+        val button: Button = findViewById(R.id.buttonRegister)
+        button.setOnClickListener {
+            val intent = Intent(this@Register, Login::class.java)
+            startActivity(intent)
+        }
+
+
+
+
+
+
+         */
+
+
+
+
+
+
+
+
 
 
     }
