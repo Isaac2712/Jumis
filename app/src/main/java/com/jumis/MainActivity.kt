aa1d2c8a941cbe4d87b86f4169411a766204c7e9
Isaac Marroquí Penalva <mmarroqui@alu.ucam.edu>
2022-10-11 13:41:43 +0200

    Cambios pequeños

--- app/src/main/java/com/jumis/MainActivity.kt
+++ app/src/main/java/com/jumis/MainActivity.kt
@@ -4,12 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
 
         /*
         Intents para las actividades
--- app/src/main/java/com/jumis/Menubottom.kt
+++ app/src/main/java/com/jumis/Menubottom.kt
@@ -2,11 +2,10 @@ package com.jumis
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class User : AppCompatActivity() {
+class Menubottom : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user)
+        setContentView(R.layout.activity_settings)
     }
 }
