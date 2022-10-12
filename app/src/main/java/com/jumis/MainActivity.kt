6e7bcc1143b4817beb9848239916d740954383b5
Isaac Marroquí Penalva <mmarroqui@alu.ucam.edu>
2022-10-12 10:23:02 +0200

    Nueva funcionalidad para añadir listas

--- app/src/main/java/com/jumis/MainActivity.kt
+++ app/src/main/java/com/jumis/MainActivity.kt
@@ -3,7 +3,10 @@ package com.jumis
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
+import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -26,23 +29,25 @@ class MainActivity : AppCompatActivity() {
         button.setOnClickListener {
             val intent = Intent(this@Register, Login::class.java)
             startActivity(intent)
+        } */
+    }
+
+    /* Codigo de comentario: #001*/
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.nueva_lista, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.añadirNuevaLista -> {
+                Toast.makeText(this, "Has hecho click en añadir nueva lista", Toast.LENGTH_LONG)
+                    .show()
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
 
-
-
-
-
-
-         */
-
-
-
-
-
-
-
-
-
-
     }
+    /*FIN 001*/
 }
