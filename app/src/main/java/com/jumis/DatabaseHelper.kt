7bb8f1c07ba20440473e571ae0f54cb96a8435f2
JuanjoLQ <jjlopezquinonero@gmail.com>
2022-10-20 20:22:23 +0200

    BBDD

---
+++ app/src/main/java/com/jumis/DatabaseHelper.kt
@@ -0,0 +1,35 @@
+package com.jumis
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DatabaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null,
+    DATABASE_VERSION) {
+    companion object {
+        // If you change the database schema, you must increment the database version.
+        const val DATABASE_VERSION = 1
+        const val DATABASE_NAME = "MySQLDataBase"
+    }
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL("CREATE TABLE Usuario (" +
+                "UID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+                "email TEXT NOT NULL, " +
+                "password TEXT NOT NULL)")
+
+        db.execSQL("CREATE TABLE Usuario2 (" +
+                "UID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+                "email TEXT NOT NULL, " +
+                "password TEXT NOT NULL)")
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+// This database is only a cache for online data, so its upgrade policy is to simply to discard the data and start over
+        db.execSQL("DROP TABLE IF EXISTS Usuario")
+        db.execSQL("DROP TABLE IF EXISTS Usuario2")
+        onCreate(db)
+    }
+    override fun onDowngrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        onUpgrade(db, oldVersion, newVersion)
+    }
+}
